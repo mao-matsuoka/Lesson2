d1d4bdba67d25caf9a4844837aff669ea93a6257
YukiMurase <yuki.timecapsule@gmail.com>
2016-07-05 10:34:35 +0900

    Initial Commit

---
+++ Lesson2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by timecapsule08 on 2016/07/05.
+//  Copyright (c) 2016年 mao.matsuoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
